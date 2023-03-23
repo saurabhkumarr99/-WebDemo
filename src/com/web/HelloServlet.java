9c55ec39a0c8ea8bf19a2c448b4139fa48a3b30a
saurabhkumarr99 <saurabhkumarr99@gmail.com>
2023-03-23 23:19:16 +0530

    Initial commit

---
+++ src/com/web/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.web;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class HelloServlet extends GenericServlet{
+
+	public void init() {
+		System.out.println("init called");
+	}
+	
+
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Inside Servlet \nHelloServlet called");
+	}
+
+	
+	public void destroy() {
+		System.out.println("destroy HelloServlet");
+	}
+}
